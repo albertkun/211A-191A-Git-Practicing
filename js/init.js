1af4c54825d18544300a5f222b57fa835afa463b
albertkun <albertk@gmx.com>
2021-07-23 01:17:38 -0700

    `lab5b` compelete

--- js/init.js
+++ js/init.js
@@ -2,9 +2,16 @@ const myMap = L.map('mapArea').setView([34.0709, -118.444], 5);
 
 const url = "https://spreadsheets.google.com/feeds/list/1upD99bKWIO68jL8MKWV67KE-_H_TVn2bCwqyQkqNsBw/oxw5dh3/public/values?alt=json"
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(myMap);
+let Esri_WorldGrayCanvas = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
+	attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
+	maxZoom: 16
+});
+
+Esri_WorldGrayCanvas.addTo(myMap)
+
+// L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+// }).addTo(myMap);
 
 fetch(url)
 	.then(response => {
@@ -16,9 +23,29 @@ fetch(url)
         }
 )
 
+let speakFluentEnglish = L.featureGroup();
+let speakOtherLanguage = L.featureGroup();
+
+let exampleOptions = {
+    radius: 4,
+    fillColor: "#ff7800",
+    color: "#000",
+    weight: 1,
+    opacity: 1,
+    fillOpacity: 0.8
+}
+
 function addMarker(data){
-    L.marker([data.lat,data.lng]).addTo(myMap).bindPopup(`<h2>${data.timestamp}</h2>`)
-    createButtons(data.lat,data.lng,data.location)
+    if(data.doyouspeakenglishfluently == "Yes"){
+        exampleOptions.fillColor = "green"
+        speakFluentEnglish.addLayer(L.circleMarker([data.lat,data.lng],exampleOptions).bindPopup(`<h2>Speak English fluently</h2>`))
+        createButtons(data.lat,data.lng,data.location)
+        }
+    else{
+        exampleOptions.fillColor = "red"
+        speakOtherLanguage.addLayer(L.circleMarker([data.lat,data.lng],exampleOptions).bindPopup(`<h2>Speak other languages</h2>`))
+        createButtons(data.lat,data.lng,data.location)
+    }
     return data.timestamp
 }
 
@@ -48,5 +75,16 @@ function formatData(theData){
           formattedData.push(formattedRow)
         }
         console.log(formattedData)
-        formattedData.forEach(addMarker)        
+        formattedData.forEach(addMarker)
+        speakFluentEnglish.addTo(myMap)
+        speakOtherLanguage.addTo(myMap)
+        let allLayers = L.featureGroup([speakFluentEnglish,speakOtherLanguage]);
+        myMap.fitBounds(allLayers.getBounds());        
 }
+
+let layers = {
+	"Speaks English": speakFluentEnglish,
+	"Speaks Other Languages": speakOtherLanguage
+}
+
+L.control.layers(null,layers).addTo(myMap)
